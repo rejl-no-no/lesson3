4d0dbeaac867566a4bb91825eee2f1367d9feaab
rejl-no-no <96285491+rejl-no-no@users.noreply.github.com>
2022-11-01 08:51:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import os, errno
+
+
+#def check_for_redirect(response):
+
+
+def save_books():
+    if not os.path.exists('books'):
+        os.makedirs('books', exist_ok=True)
+
+    os.chdir('books')
+
+    counter = 1
+
+    while counter <= 10:
+
+        url = "https://tululu.org/txt.php?id=" + str(counter)
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        if response.history == []:
+            filename = 'id' + str(counter) + '.txt'
+
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            
+
+        counter+=1
+
+
+#try:
+
+#except:
+
+
+if __name__ == "__main__":
+    save_books()
+
+
+
+
+
+
