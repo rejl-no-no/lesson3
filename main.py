0829f5d5be2e79c5c5d8d44a6113c2780e460d53
rejl-no-no <96285491+rejl-no-no@users.noreply.github.com>
2022-12-14 16:28:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,31 +16,34 @@ def check_for_redirect(response):
         raise HTTPError()
 
 
-def download_txt(title_name, book_id, download_response, folder="books/"):
+def download_txt(title_name, book_id, download_response, dest_folder, folder_txt="books/"):
 
-    os.makedirs(folder, exist_ok=True)
+    os.makedirs(f'{dest_folder}{folder_txt}', exist_ok=True)
 
-    with open(f'{folder}{book_id}. {title_name}.txt', "wb") as file:
+    with open(f'{dest_folder}{folder_txt}{book_id}. {title_name}.txt', "wb") as file:
         file.write(download_response.content)
 
 
-def download_image(image_url, image_name, folder="images/"):
+def download_image(image_url, image_name, dest_folder, folder_img="images/"):
 
-    os.makedirs(folder, exist_ok=True)
+    os.makedirs(f'{dest_folder}{folder_img}', exist_ok=True)
 
     response = requests.get(image_url)
     response.raise_for_status()
 
-    with open(f'{folder}{image_name}', "wb") as file:
+    with open(f'{dest_folder}{folder_img}{image_name}', "wb") as file:
         file.write(response.content)
 
 
 def parse_book_page(soup, page_url):
 
-    body_text = soup.find("h1").text.split("::")
-    title_name, author_name = body_text
+    body_text_selector = ('h1')
+    for body_text in soup.select(body_text_selector):
+        title_name, author_name = body_text.text.split("::")
 
-    short_image_url = soup.find(class_="bookimage").find("img")["src"]
+    image_url_selector = ('.bookimage img')
+    for image_url in soup.select(image_url_selector):
+        short_image_url = image_url['src']
 
     image_url = urljoin(page_url, short_image_url)
     splitted_image_url = urllib.parse.urlsplit(image_url)
@@ -65,17 +68,34 @@ def parse_book_page(soup, page_url):
     }
 
 
+def create_json(page_content):
+    books_description = json.dumps(json_content, ensure_ascii=False,  indent=2)
+
+    with open(f"{args.dest_folder}{args.json_path}page_content_json.json", "w", encoding='utf8') as my_file:
+        my_file.write(books_description)
+
+    with open(f"{args.dest_folder}{args.json_path}page_content_json.json", "w", encoding='utf8') as my_file:
+        json_content.append(page_content)
+        my_file.write(books_description)
+
+
+
 if __name__ == "__main__":
 
     url = "https://tululu.org/"
     json_content = []
+
     parser = argparse.ArgumentParser(description="Парсер онлайн библиотеки")
     parser.add_argument("--start_id", default = 1, type = int, help="С какой книги начинать скачивание")
-    parser.add_argument("--end_id", default = 3, type = int, help="До какой книги качать")
+    parser.add_argument("--end_id", default = 702, type = int, help="До какой книги качать")
+    parser.add_argument("--dest_folder", default = '', help="Путь к каталогу с резул. парсинга")
+    parser.add_argument("--skip_imgs", action = 'store_false', help="Не скачивать изображения")
+    parser.add_argument("--skip_txt", action = 'store_false', help="Не скачивать текст книги")
+    parser.add_argument("--json_path", default = '', help="Путь к .json файлу")
     args = parser.parse_args()
 
 
-    for count in range(1, 3):
+    for count in range(args.start_id, (args.end_id)):
 
         fantastic_url = urljoin(url, f"l55/{count}/")
 
@@ -83,20 +103,22 @@ if __name__ == "__main__":
         url_response.raise_for_status()
         fantastic_soup = BeautifulSoup(url_response.text, "lxml")
 
-        pages_id = fantastic_soup.select(".bookimage")
+        pages_id = fantastic_soup.select(".bookimage a")
         
         for page in pages_id:
-            page_url = urljoin(url, page.find("a")["href"])
 
+            page_url = urljoin(url, page['href'])
+            
             try:
                 url_response = requests.get(page_url)
                 url_response.raise_for_status()
 
                 soup = BeautifulSoup(url_response.text, "lxml")
 
-                #book_id_selector = "attrs=bookid"
-                #print(soup.select(book_id_selector))
-                book_id = soup.find(attrs={"name": "bookid"})['value']
+                book_id_selector = '.r_comm input[name="bookid"]'
+                for id_selector in soup.select(book_id_selector):
+                    book_id = (id_selector['value'])
+
                 download_payload = {"id":f"{book_id}"}
 
                 download_response = requests.get(urljoin(url,"txt.php"), params = download_payload)
@@ -107,18 +129,11 @@ if __name__ == "__main__":
 
                 page_content = parse_book_page(soup, page_url)
 
-                books_description = json.dumps(json_content, ensure_ascii=False,  indent=2)
-
-                with open("page_content_json.json", "w", encoding='utf8') as my_file:
-                    my_file.write(books_description)
-
-                with open("page_content_json.json", "w", encoding='utf8') as my_file:
-                    json_content.append(page_content)
-                    my_file.write(books_description)
-
-
-                download_txt(page_content["title_name"], book_id, download_response)
-                download_image(page_content["image_url"], page_content["image_name"])
+                create_json(page_content)
+                if args.skip_txt:
+                    download_txt(page_content["title_name"], book_id, download_response, args.dest_folder)
+                if args.skip_imgs:
+                    download_image(page_content["image_url"], page_content["image_name"], args.dest_folder)
 
             except requests.exceptions.ConnectionError:
                 print("Сбой сети!")
