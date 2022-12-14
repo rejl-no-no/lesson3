671cb1c8cbfcf30f1f00c58748c1b522823e77a2
rejl-no-no <96285491+rejl-no-no@users.noreply.github.com>
2022-12-14 17:57:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,32 +18,32 @@ def check_for_redirect(response):
 
 def download_txt(title_name, book_id, download_response, dest_folder, folder_txt="books/"):
 
-    os.makedirs(f'{dest_folder}{folder_txt}', exist_ok=True)
+    os.makedirs(f"{dest_folder}{folder_txt}", exist_ok=True)
 
-    with open(f'{dest_folder}{folder_txt}{book_id}. {title_name}.txt', "wb") as file:
+    with open(f"{dest_folder}{folder_txt}{book_id}. {title_name}.txt", "wb") as file:
         file.write(download_response.content)
 
 
 def download_image(image_url, image_name, dest_folder, folder_img="images/"):
 
-    os.makedirs(f'{dest_folder}{folder_img}', exist_ok=True)
+    os.makedirs(f"{dest_folder}{folder_img}", exist_ok=True)
 
     response = requests.get(image_url)
     response.raise_for_status()
 
-    with open(f'{dest_folder}{folder_img}{image_name}', "wb") as file:
+    with open(f"{dest_folder}{folder_img}{image_name}", "wb") as file:
         file.write(response.content)
 
 
 def parse_book_page(soup, page_url):
 
-    body_text_selector = ('h1')
+    body_text_selector = ("h1")
     for body_text in soup.select(body_text_selector):
         title_name, author_name = body_text.text.split("::")
 
-    image_url_selector = ('.bookimage img')
+    image_url_selector = (".bookimage img")
     for image_url in soup.select(image_url_selector):
-        short_image_url = image_url['src']
+        short_image_url = image_url["src"]
 
     image_url = urljoin(page_url, short_image_url)
     splitted_image_url = urllib.parse.urlsplit(image_url)
@@ -71,10 +71,10 @@ def parse_book_page(soup, page_url):
 def create_json(page_content):
     books_description = json.dumps(json_content, ensure_ascii=False,  indent=2)
 
-    with open(f"{args.dest_folder}{args.json_path}page_content_json.json", "w", encoding='utf8') as my_file:
+    with open(f"{args.dest_folder}{args.json_path}page_content_json.json", "w", encoding="utf8") as my_file:
         my_file.write(books_description)
 
-    with open(f"{args.dest_folder}{args.json_path}page_content_json.json", "w", encoding='utf8') as my_file:
+    with open(f"{args.dest_folder}{args.json_path}page_content_json.json", "w", encoding="utf8") as my_file:
         json_content.append(page_content)
         my_file.write(books_description)
 
@@ -88,10 +88,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Парсер онлайн библиотеки")
     parser.add_argument("--start_id", default = 1, type = int, help="С какой книги начинать скачивание")
     parser.add_argument("--end_id", default = 702, type = int, help="До какой книги качать")
-    parser.add_argument("--dest_folder", default = '', help="Путь к каталогу с резул. парсинга")
-    parser.add_argument("--skip_imgs", action = 'store_false', help="Не скачивать изображения")
-    parser.add_argument("--skip_txt", action = 'store_false', help="Не скачивать текст книги")
-    parser.add_argument("--json_path", default = '', help="Путь к .json файлу")
+    parser.add_argument("--dest_folder", default = "", help="Путь к каталогу с резул. парсинга")
+    parser.add_argument("--skip_imgs", action = "store_false", help="Не скачивать изображения")
+    parser.add_argument("--skip_txt", action = "store_false", help="Не скачивать текст книги")
+    parser.add_argument("--json_path", default = "", help="Путь к .json файлу")
     args = parser.parse_args()
 
 
@@ -107,7 +107,7 @@ if __name__ == "__main__":
         
         for page in pages_id:
 
-            page_url = urljoin(url, page['href'])
+            page_url = urljoin(url, page["href"])
             
             try:
                 url_response = requests.get(page_url)
@@ -115,9 +115,9 @@ if __name__ == "__main__":
 
                 soup = BeautifulSoup(url_response.text, "lxml")
 
-                book_id_selector = '.r_comm input[name="bookid"]'
+                book_id_selector = ".r_comm input[name="bookid"]"
                 for id_selector in soup.select(book_id_selector):
-                    book_id = (id_selector['value'])
+                    book_id = (id_selector["value"])
 
                 download_payload = {"id":f"{book_id}"}
 
